75058993188c499ba195f7826d6d1ba5be41c8b4
YvesJaques <ymoraisjaques@gmail.com>
2024-01-29 22:29:21 -0300

    Setup mongoose connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = require('./app');[]
@@ -11,7 +12,21 @@ const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/nasa-api';
 
 const server = http.createServer(app)
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!')
+})
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+})
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    })
     await loadPlanetsData();
 
     server.listen(PORT, () => {
