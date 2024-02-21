e1368059a383e9816d55277eb4d4706022b98dce
YvesJaques <ymoraisjaques@gmail.com>
2024-02-20 23:26:46 -0300

    Moved db connection to a independent file

--- server/src/server.js
+++ server/src/server.js
@@ -1,27 +1,16 @@
 const http = require('http');
-const mongoose = require('mongoose');
-require('dotenv').config();
 
 const app = require('./app');[]
 
-const { loadPlanetsData } = require('./models/planets.model')
+const { loadPlanetsData } = require('./models/planets.model');
+const { connectDb } = require('./services/nosqldb');
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/nasa-api';
-
 const server = http.createServer(app)
 
-mongoose.connection.once('open', () => {
-    console.log('MongoDB connection ready!')
-})
-
-mongoose.connection.on('error', (err) => {
-    console.error(err);
-})
-
 async function startServer() {
-    await mongoose.connect(MONGO_URL)
+    await connectDb()
 
     await loadPlanetsData();
 
