44e77cc4d5c91bbb242d499e12d9f94df874e742
YvesJaques <ymoraisjaques@gmail.com>
2024-02-23 22:12:11 -0300

    Minimixing API Load

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -17,11 +17,12 @@ saveLaunch(launch)
 
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query'
 
-async function loadLaunchData() {
-    console.log('Dowloading launch data');
+async function populateLaunches() {
+    console.log('Dowloading launch data...');
     const response = await axios.post(SPACEX_API_URL, {
         query: {},
         options: {
+            pagination: false,
             populate: [
                 {
                     path: 'rocket',
@@ -58,12 +59,30 @@ async function loadLaunchData() {
 
         console.log(`${launch.flightNumber} ${launch.mission}`);
 
+        //TODO: populate launches collection...
+
     }
 }
 
+async function loadLaunchData() {
+    const firstLaunch = await findLaunch({
+        flightNumber: 1,
+        rocket: 'Falcon 1',
+        mission: 'FalconSat'
+    })
+    if (firstLaunch) {
+        console.log('Launch data already loaded!');
+    } else {
+        await populateLaunches();
+    }
+}
+
+async function findLaunch(filter) {
+    return await launchesDatabase.findOne(filter);
+}
 
 async function existsLaunchWithId(launchId) {
-    return await launchesDatabase.findOne({
+    return await findLaunch({
         flightNumber: launchId
     })
 }
