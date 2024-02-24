19f9b64b4c834033a4425bbf676eac41f00a7d76
YvesJaques <ymoraisjaques@gmail.com>
2024-02-24 09:55:15 -0300

    Persisting launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -13,8 +13,6 @@ const launch = {
     success: true, //success
 };
 
-saveLaunch(launch)
-
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query'
 
 async function populateLaunches() {
@@ -38,7 +36,12 @@ async function populateLaunches() {
                 }
             ]
         }
-    }); 
+    });
+
+    if (response.status !== 200) {
+        console.log('Problem downloading launch data');
+        throw new Error('Launch data download failed')
+    }
 
     const launchDocs = response.data.docs
     for (const launchDoc of launchDocs) {
@@ -59,7 +62,7 @@ async function populateLaunches() {
 
         console.log(`${launch.flightNumber} ${launch.mission}`);
 
-        //TODO: populate launches collection...
+        await saveLaunch(launch)
 
     }
 }
@@ -103,14 +106,6 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
-    const planet = await planets.findOne({
-        keplerName: launch.target,
-    })
-
-    if (!planet) {
-        throw new Error('No matching planet found')
-    }
-
     await launchesDatabase.findOneAndUpdate({
         flightNumber: launch.flightNumber,
 
@@ -120,6 +115,14 @@ async function saveLaunch(launch) {
 }
 
 async function scheduleNewLaunch(launch) {
+    const planet = await planets.findOne({
+        keplerName: launch.target,
+    })
+
+    if (!planet) {
+        throw new Error('No matching planet found')
+    }
+
     const newFlightNumber = await getLatestFlightNumber() + 1;
 
     const newLaunch = Object.assign(launch, {
